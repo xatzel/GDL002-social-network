94b48eb81538cd5c6f0a7e80ac3471b18c61b025
shareazc <shareni.azcarraga@gmail.com>
2019-03-22 08:27:56 -0600

    Se actualizó base de datos y se comenzó el testeo para manipular la data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
-'use strict';
+"use strict";
+
+/* ---------------- FETCHING DATA ---------------- */
+
+fetch("https://raw.githubusercontent.com/shareazc/GDL002-social-network/master/src/data/services.json")
+.then((data) => {
+  // convertimos nuestra data a JSON
+  return data.json();
+}).then((dataAsJSON) => {
+  // cualquier operación con la data ya procesada
+  console.log(Object.values(dataAsJSON));
+});
+
+/* ------------------ SELECT DIVS AS BUTTONS ----------------- */
+
+const searchBtn = document.querySelector("#btnsearch");
+const searchBar = document.querySelector("#searchbar");
+const menuBtn = document.querySelector("#btnmenu");
+
+/* ------------ FIND PLACE BY NAME ----------- */ 
+//Recibir el nombre del pokemon a buscar
+const getSearch = () => {
+    return document.getElementById("searchbar").value;
+};
+
+/* --------------- EVENT LISTENERS -------------- */
+
+
+
+
+/* ----------- PRINT ON CARD ---------------- */
+
+//div "name"
+//div "scheduleinfo"
+//div "telephoneinfo"
+//div "addressinfo"
+
