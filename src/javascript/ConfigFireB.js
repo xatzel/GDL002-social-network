a5623cd2dcf7701bc5dc5c55b4645055382c02b7
shareazc <shareni.azcarraga@gmail.com>
2019-04-05 13:25:38 -0600

    Se modificaron estilos

--- src/javascript/ConfigFireB.js
+++ src/javascript/ConfigFireB.js
@@ -2,13 +2,16 @@
 // por que mi apikey tiene informacion confidencial
 // Initialize Firebase
 let config = {
-	apiKey: 'AIzaSyDwDFXpRkKoNxX_EOWwVIAHVo654hR0i8c',
-	authDomain: 'social-network-ef8fe.firebaseapp.com',
-	databaseURL: 'https://social-network-ef8fe.firebaseio.com',
-	projectId: 'social-network-ef8fe',
-	storageBucket: 'gs://social-network-ef8fe.appspot.com/',
-	messagingSenderId: '676810242509'
+    apiKey: 'AIzaSyDwDFXpRkKoNxX_EOWwVIAHVo654hR0i8c',
+    authDomain: 'social-network-ef8fe.firebaseapp.com',
+    databaseURL: 'https://social-network-ef8fe.firebaseio.com',
+    projectId: 'social-network-ef8fe',
+    storageBucket: 'gs://social-network-ef8fe.appspot.com/',
+    messagingSenderId: '676810242509'
 };
 
 firebase.initializeApp(config);
 const db = firebase.firestore();
+db.settings({
+    timestampsInSnapshots: true
+});
